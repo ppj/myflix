9b26a3c9dc3b348151803e440785ee7595562ef2
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-01-20 17:28:20 +1100

    added category show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Myflix::Application.routes.draw do
   get '/home', to: 'videos#index'
 
   get '/video/:id', to: 'videos#show', as: 'video'
+
+  get '/genre/:id', to: 'categories#show', as: 'category'
 end

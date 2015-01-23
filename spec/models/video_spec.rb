7b329d9e096a2f3bb51200378164b0c632efa774
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-01-23 13:12:02 +1100

    added spec to check it belongs to a category

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,14 @@ describe Video do
     video.save
     expect(Video.first).to eq(video)
   end
+
+  it 'belongs to a category' do
+    video = Video.create(title: 'Big Bang Theory', description: 'A comedy series featuring a bunch of scientist-friends and their girlfriends.')
+    cat = Category.create(name: 'TV Sitcom')
+    cat.videos << video
+    # Some alternatives to the above
+    # video.category_id = cat.id
+    # video.save
+    expect(video.category).to eq(cat)
+  end
 end

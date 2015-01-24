a4a90f33365861856126d7bb240a01ac08ea258d
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-01-24 14:56:46 +1100

    no need to check whether the model is being saved

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it 'saves successfully' do
-    cat = Category.new(name: 'Drama')
-    cat.save
-    expect(Category.first).to eq(cat)
-  end
-
   it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it 'saves successfully' do
-    video = Video.new(title: 'Test Video', description: 'Lorem Ipsum Dipsum Dimsum')
-    video.save
-    expect(Video.first).to eq(video)
-  end
-
   it {should belong_to(:category)}
-
   it { should validate_presence_of(:title) }
-
   it { should validate_presence_of(:description) }
-
 end

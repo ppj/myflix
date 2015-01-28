1ab5bdc92535871dbe9c050c4fcf50c92d63f602
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-01-28 16:23:05 +1100

    implemented video search by title

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_string)
+    Video.where('title LIKE ?', "%#{search_string}%").all
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 describe Video do
   it {should belong_to(:category)}
+
   it { should validate_presence_of(:title) }
+
   it { should validate_presence_of(:description) }
+
+  describe '#search_by_title' do
+    it 'cannot find videos by words in title' do
+      video = Video.create(title: 'How I Met Your Mother', description: 'Sitcom Sitcom')
+      expect(Video.search_by_title('family')).to eq([])
+    end
+
+    it 'finds videos by words in title' do
+      video1 = Video.create(title: 'How I Met Your Mother', description: 'Sitcom')
+      video2 = Video.create(title: 'Mother', description: 'Sitcom Sitcom')
+      expect(Video.search_by_title('Mother')).to eq([video1, video2])
+    end
+  end
+
 end

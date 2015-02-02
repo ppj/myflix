74ace65495b0cac09b254c76402c3e27cc9d37d7
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-02 11:41:55 +1100

    redefined user model class based on test-first

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
 
-  validates :fullname, length: {minimum: 2}
-  validates :password, on: :create, length: {minimum: 3}
+  validates :password, length: {minimum: 3}
+  validates :email, length: {minimum: 5}
   validates :email, uniqueness: true
+  validates :fullname, length: {minimum: 2}
 end

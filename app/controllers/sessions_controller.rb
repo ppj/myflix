09c7780e25ace91eb71397cdbea85081b407bbce
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-03 12:57:44 +1100

    enabled front, sign-in & sign-up pages & backend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
-  def front; end
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in.'
+      redirect_to root_path
+    else
+      flash.now[:danger] = 'The username or password you entered is incorrect.'
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  root to: 'sessions#front'
+  root to: 'pages#front'
 
   resources :videos, only: [:show] do
     collection do
       get 'search', to: 'videos#search'
     end
   end
+
+  get  'sign_in' , to: 'sessions#new'
+  post 'sign_in',  to: 'sessions#create'
+  get  'register', to: 'users#new'
+
+  resources :users, only: [:create]
 
   get '/genre/:id', to: 'categories#show', as: 'category'
 end

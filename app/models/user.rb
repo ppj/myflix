14a5482c4c21a2630f21dbdb01cadf773fec3908
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-10 17:08:45 +1100

    combined email validations into one

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :password, length: {minimum: 3}
-  validates :email, length: {minimum: 5}
-  validates :email, uniqueness: true
+  validates :email, length: {minimum: 5}, uniqueness: true
   validates :fullname, length: {minimum: 2}
 end

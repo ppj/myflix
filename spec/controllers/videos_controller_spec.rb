7bd2313ffdbc8d56d9b326d0c1249386c9d0b21d
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-17 16:18:51 +1100

    added tests to Videos#show

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
-    let(:user) { Fabricate(:user) }
-    it "finds a Video based on given name" do
-      session[:user_id] = user.id
-      monk = Video.create(title: "Monk", description: "a drama series")
-      get :show, id: monk.id
-      expect(assigns(:video)).to eq(monk)
+    let(:test_user)  { Fabricate(:user) }
+    let(:test_video) { Fabricate(:video) }
+
+    it "redirects to home page when no user is signed in" do
+      get :show, id: test_video.id
+      expect(response).to redirect_to(root_path)
     end
-    it "renders the show template"
+
+    it "finds a Video based on given name" do
+      session[:user_id] = test_user.id
+      get :show, id: test_video.id
+      expect(assigns(:video)).to eq(test_video)
+    end
+
+    it "renders the show template" do
+      session[:user_id] = test_user.id
+      get :show, id: test_video.id
+      expect(response).to render_template(:show)
+    end
   end
 end
--- spec/fabricators/video_fabricator.rb
+++ spec/fabricators/video_fabricator.rb
@@ -1,4 +1,4 @@
 Fabricator(:video) do
-  title { Faker::Lorem.words 2 }
-  description { Faker::Lorem.words 5 }
+  title { Faker::Lorem.words(2).join(" ") }
+  description { Faker::Lorem.words(4).join(" ") }
 end

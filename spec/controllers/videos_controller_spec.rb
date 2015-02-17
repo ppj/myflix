a8615c3ad1d865d61e5abc1613f91e7b5cf343b5
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-17 17:18:00 +1100

    added Videos#search specs

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe VideosController do
+  let(:test_user)  { Fabricate(:user) }
+  let(:test_video) { Fabricate(:video) }
   describe "GET show" do
-    let(:test_user)  { Fabricate(:user) }
-    let(:test_video) { Fabricate(:video) }
 
-    it "redirects to home page when no user is signed in" do
+    it "redirects to root when no user is signed in" do
       get :show, id: test_video.id
       expect(response).to redirect_to(root_path)
     end
@@ -22,4 +22,24 @@ describe VideosController do
       expect(response).to render_template(:show)
     end
   end
+
+  describe "GET create" do
+    it "redirects to root when no user is signed in" do
+      get :search, search_string: 'A' 
+      expect(response).to redirect_to(root_path)
+    end
+    
+    it "finds a Video based on given name" do
+      session[:user_id] = test_user.id
+      get :search, search_string: test_video.title.split.first
+      expect(assigns(:videos)).to eq([test_video])
+    end
+
+    it "renders the show template" do
+      session[:user_id] = test_user.id
+      get :search, search_string: test_video.title.split.first
+      expect(response).to render_template(:search)
+    end
+  end
+
 end

4b6a92a904d36d761af28e7d10f0f800fde0809f
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-19 00:46:02 +1100

    modified rspec output to run tests as ordered

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,8 +42,11 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = "defined"
 
+  # Show the output in documentation mode
+  # Set it to 'progress', 'html', 'json', 'textmate' ... see help for details
+  config.formatter = 'documentation'
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.

5c9d8006e595ff7b2f76a2a6ca90ec99bf6904f6
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-20 22:12:58 +1100

    video has many reviews and also an average rating

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,21 @@
 class Video < ActiveRecord::Base
-  belongs_to :category
+  before_validation :update_rating
 
   validates_presence_of :title, :description
+  belongs_to :category
+  has_many :reviews
+
+  validates_numericality_of :rating, greater_than_or_equal_to: 1
+  validates_numericality_of :rating, less_than_or_equal_to: 5
 
   def self.search_by_title(search_string)
     return [] if search_string.blank?
     Video.where('title LIKE ?', "%#{search_string}%").order('created_at DESC')
+  end
+
+  # private
+
+  def update_rating
+    self.rating ||= 1.0 unless self.reviews.any?
   end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe Video do
-  it {should belong_to(:category)}
-
+  it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
-
   it { should validate_presence_of(:description) }
+  it { should have_many(:reviews) }
+  it do
+    should validate_numericality_of(:rating)
+       .is_greater_than_or_equal_to(1)
+  end
+  it do
+    should validate_numericality_of(:rating)
+       .is_less_than_or_equal_to(5)
+  end
 
   describe '#search_by_title' do
     it 'returns empty array if no video found' do
@@ -36,7 +43,12 @@ describe Video do
       video2 = Video.create(title: 'Mother', description: 'Sitcom Sitcom')
       expect(Video.search_by_title("    ")).to eq([])
     end
-
   end
 
+  describe "#update_rating" do
+    it "sets rating on creation of new video" do
+      video1 = Fabricate(:video)
+      expect(video1.rating).to eq(1.0)
+    end
+  end
 end

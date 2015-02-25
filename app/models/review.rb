cf0dd360a40f722e46c7a160a479a7e42f206a7a
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-02-25 22:56:12 +1100

    ensure that a review has the associated user & video

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,13 @@
 class Review < ActiveRecord::Base
   after_save :update_video_rating!
 
-  validates_inclusion_of :rating, in: 1..5
-  validates :body, length: { minimum: 10 }
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :video
+
+  validates_presence_of :user_id
+  validates_presence_of :video_id
+  validates_inclusion_of :rating, in: 1..5
+  validates :body, length: { minimum: 10 }
 
   private
 
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,17 +1,26 @@
 require 'spec_helper'
 
 describe Review do
-  it { should validate_inclusion_of(:rating).in_range(1..5) }
-  it { should validate_length_of(:body).is_at_least(10) }
   it do
     should belong_to(:creator).
       class_name('User').
       with_foreign_key('user_id')
   end
+
   it { should belong_to(:video) }
+
+  it { should validate_presence_of(:user_id) }
+
+  it { should validate_presence_of(:video_id) }
+
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
+
+  it { should validate_length_of(:body).is_at_least(10) }
+
   it "should update video rating after saving successfully" do
+    test_user  = Fabricate(:user)
     test_video = Fabricate(:video)
     expect_any_instance_of(Video).to receive(:update_rating!).with(no_args)
-    Review.create(Fabricate.attributes_for(:review, rating: 3, video: test_video))
+    test_user.reviews.create(Fabricate.attributes_for(:review, video: test_video))
   end
 end

10c83bc5735863e75aa5b1ab63c2611002b08d2d
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-01 00:16:24 +1100

    added specs for thoroughness & cleaned up :white_check_mark:

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe ReviewsController do
+  let(:test_video) { Fabricate(:video) }
+
   describe "POST create" do
     context "with authenticated user" do
+      let(:current_user) { Fabricate(:user) }
+      before { session[:user_id] = current_user.id }
+
       context "with valid review contents" do
         before do
-          session[:user_id] = Fabricate(:user).id
-          post :create, { video_id: Fabricate(:video).id, review: Fabricate.attributes_for(:review) }
+          post :create, { video_id: test_video.id, review: Fabricate.attributes_for(:review) }
         end
 
         it "creates new review" do
@@ -14,11 +18,19 @@ describe ReviewsController do
         end
 
         it "sets the @video to current video" do
-          expect(assigns(:video)).to eq(Review.first.video)
+          expect(assigns(:video)).to eq(test_video)
         end
 
-        it "then redirects to the video show page" do
-          expect(response).to redirect_to(video_path Review.first.video)
+        it "associates the review with the current video" do
+          expect(Review.first.video).to eq(test_video)
+        end
+
+        it "associates the review with the current user" do
+          expect(Review.first.creator).to eq(current_user)
+        end
+
+        it "redirects to the video show page" do
+          expect(response).to redirect_to(video_path test_video)
         end
 
         it "notifies the user of successful review creation" do
@@ -28,9 +40,9 @@ describe ReviewsController do
 
       context "with invalid review contents" do
         before do
-          session[:user_id] = Fabricate(:user).id
-          post :create, { video_id: Fabricate(:video).id, review: {body: '2short', rating: 0} }
+          post :create, { video_id: test_video.id, review: {body: '2short', rating: 0} }
         end
+
         it "sets @review to a new Review" do
           expect(assigns(:review)).to be_a_new(Review)
         end
@@ -39,7 +51,11 @@ describe ReviewsController do
           expect(Review.count).to eq(0)
         end
 
-        it "re-renders the review form" do
+        it "sets @video to the current video" do
+          expect(assigns(:video)).to eq(test_video)
+        end
+
+        it "re-renders the video page with review form" do
           expect(response).to render_template('videos/show')
         end
       end
@@ -47,7 +63,7 @@ describe ReviewsController do
 
     context "with unauthenticated user" do
       it "redirects to home page" do
-        post :create, { video_id: Fabricate(:video).id, review: Fabricate.attributes_for(:review) }
+        post :create, { video_id: test_video.id, review: Fabricate.attributes_for(:review) }
         expect(response).to redirect_to(root_path)
       end
     end

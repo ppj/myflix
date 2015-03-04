37e813766df6649e0a0cd2ec33114df555b95b34
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-04 15:56:01 +1100

    added get route to display user queue

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,11 @@ Myflix::Application.routes.draw do
     end
   end
 
-  get  'sign_in' , to: 'sessions#new'
-  get  'sign_out', to: 'sessions#destroy'
-  get  'home',     to: 'pages#home'
-
-  get  'register', to: 'users#new'
+  get 'sign_in' , to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
+  get 'home',     to: 'pages#home'
+  get 'register', to: 'users#new'
+  get 'my_queue', to: 'queue_items#index'
 
   resources :sessions, only: [:create]
   resources :users, only: [:create]

805bb4c95866169350310b6d179ad9d7906159c0
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-05 12:54:03 +1100

    a video can be a queue_item only once for a user

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,6 +1,7 @@
 class QueueItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :video
+  validates_uniqueness_of :video_id, scope: :user_id
 
   delegate :title, to: :video, prefix: :video
 

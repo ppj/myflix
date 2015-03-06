71c7871251490ef2137d1888d0d6f8fc98425966
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-06 23:52:16 +1100

    add video to user's queue

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -4,4 +4,9 @@ class QueueItemsController < ApplicationController
   def index
     @queue_items = current_user.queue_items
   end
+
+  def new
+    current_user.queue_items.create(video: Video.find(params[:video_id]))
+    redirect_to my_queue_path
+  end
 end
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -1,16 +1,54 @@
 require 'spec_helper'
 
 describe QueueItemsController do
-  it "sets @queue_items to the queue items for an authenticated user" do
-    bob = Fabricate(:user)
-    queue_item1 = Fabricate(:queue_item, user: bob)
-    queue_item2 = Fabricate(:queue_item, user: bob)
-    session[:user_id] = bob.id
-    get :index
-    expect(assigns(:queue_items)).to match_array([queue_item1, queue_item2])
+  describe "GET index" do
+    it "sets @queue_items to the queue items for an authenticated user" do
+      bob = Fabricate(:user)
+      queue_item1 = Fabricate(:queue_item, user: bob)
+      queue_item2 = Fabricate(:queue_item, user: bob)
+      session[:user_id] = bob.id
+      get :index
+      expect(assigns(:queue_items)).to match_array([queue_item1, queue_item2])
+    end
+
+    it "redirects to front page for unauthenticated user" do
+      get :index
+      expect(response).to redirect_to(root_path)
+    end
   end
-  it "redirects to front page for unauthenticated user" do
-    get :index
-    expect(response).to redirect_to(root_path)
+
+  describe "GET new" do
+    let(:current_user) { Fabricate(:user) }
+    let(:video) { Fabricate(:video) }
+
+    context "for an authenticated user" do
+      before do
+        session[:user_id] = current_user.id
+      end
+
+      it "adds the current video as the current_user's queue_item" do
+        get :new, video_id: video.id
+        expect(current_user.queue_items.map(&:video)).to include(video)
+      end
+
+      it "adds a queue_item to the end of the queue" do
+        current_user.queue_items.create(video: video)
+        get :new, video_id: Fabricate(:video).id
+        expect(current_user.queue_items.last.position).to be(current_user.queue_items.map(&:position).max)
+      end
+
+      it "redirects to the user's queue index page" do
+        get :new, video_id: video.id
+        expect(response).to redirect_to(my_queue_path)
+      end
+    end
+
+    context "for an unuthenticated user" do
+      it "redirects to the front page" do
+        get :new, video_id: video.id
+        expect(response).to redirect_to(root_path)
+      end
+
+    end
   end
 end

1b13e5ae11822866fbdf8a1493e0a5bd25faa622
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-24 00:37:15 +1100

    added padding

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe User do
   it { should have_secure_password }
+
   it { should validate_length_of(:password).is_at_least(3) }
+
   it { should validate_length_of(:email).is_at_least(5) }
+
   it { should validate_uniqueness_of(:email) }
+
   it { should validate_length_of(:fullname).is_at_least(2) }
+
   it { should have_many(:reviews) }
+
   it { should have_many(:queue_items).order(:position) }
 end

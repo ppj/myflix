9ecd032fefa94db75488e4f3053630e95c2369fe
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-26 10:27:12 +1100

    minor refactoring

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 feature "User Signs In" do
-  background do
-    Fabricate(:user, email: "user@sample.com", password: "pwd")
-  end
+  given(:bob) { Fabricate(:user) }
 
   scenario "with existing credentials" do
     visit root_path
     click_on "Sign In"
-    fill_in "Email Address", with: "user@sample.com"
-    fill_in "Password", with: "pwd"
+    fill_in "Email Address", with: bob.email
+    fill_in "Password", with: bob.password
     click_button "Sign in"
     expect(page).to have_content "You have successfully logged in."
+    expect(page).to have_content bob.fullname
   end
 end

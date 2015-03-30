15a8298fdf8f55e06bde41943b4f6bcce2fc2379
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-31 00:43:14 +1100

    add xpath based search for position and abstract code for readability

--- spec/features/user_interaction_with_queue_spec.rb
+++ spec/features/user_interaction_with_queue_spec.rb
@@ -6,34 +6,57 @@ feature "User Interaction With Queue" do
     bob = Fabricate(:user)
     category = Fabricate(:category)
     video1 = category.videos.create(Fabricate.attributes_for(:video))
+    video2 = category.videos.create(Fabricate.attributes_for(:video))
+    video3 = category.videos.create(Fabricate.attributes_for(:video))
+
     sign_in_user bob
 
-    find("a[href='/videos/#{video1.id}']").click
-    click_link "+ My Queue"
-    expect(page).to have_content(video1.title)
+    add_video_to_queue video1
+    expect_to_find video1.title
 
     click_link video1.title
-    expect(page).to have_content(video1.title)
-    expect(page).to have_no_content("+ My Queue")
+    expect_to_find video1.title
+    expect_to_not_find "+ My Queue"
 
-    video2 = category.videos.create(Fabricate.attributes_for(:video))
-    visit home_path
-    find("a[href='/videos/#{video2.id}']").click
-    click_link "+ My Queue"
+    add_video_to_queue video2
+    add_video_to_queue video3
 
-    video3 = category.videos.create(Fabricate.attributes_for(:video))
-    visit home_path
-    find("a[href='/videos/#{video3.id}']").click
-    click_link "+ My Queue"
+    fill_in_video_position video1, 5
+    fill_in_video_position video2, 2
+    fill_in_video_position video3, 3
 
-    find("input[data-video-id='#{video1.id}']").set(5)
-    find("input[data-video-id='#{video2.id}']").set(4)
-    find("input[data-video-id='#{video3.id}']").set(2)
-    click_on "Update Instant Queue"
+    update_queue
 
-    expect(find("input[data-video-id='#{video1.id}']").value).to eq("3")
-    expect(find("input[data-video-id='#{video2.id}']").value).to eq("2")
-    expect(find("input[data-video-id='#{video3.id}']").value).to eq("1")
+    expect_video_position video1, 3
+    expect_video_position video2, 1
+    expect_video_position video3, 2
   end
 end
 
+def add_video_to_queue(video)
+    visit home_path
+    find("a[href='/videos/#{video.id}']").click
+    click_link "+ My Queue"
+end
+
+def fill_in_video_position(video, position)
+  within(:xpath, "//tr[contains(., '#{video.title}')]") do
+    fill_in "queue_items[][position]", with: position
+  end
+end
+
+def expect_video_position(video, position)
+    expect(find(:xpath, "//tr[contains(., '#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
+end
+
+def expect_to_find(text)
+  expect(page).to have_content(text)
+end
+
+def expect_to_not_find(text)
+  expect(page).to have_no_content(text)
+end
+
+def update_queue
+  click_on "Update Instant Queue"
+end

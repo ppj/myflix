47b4e8a348120566aefcfa804d5cf9caf07605f2
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-03-30 23:35:29 +1100

    work with a data attribute instead of id

--- spec/features/user_interaction_with_queue_spec.rb
+++ spec/features/user_interaction_with_queue_spec.rb
@@ -5,34 +5,35 @@ feature "User Interaction With Queue" do
   scenario "user adds videos to queue and reorders them" do
     bob = Fabricate(:user)
     category = Fabricate(:category)
-    video = category.videos.create(Fabricate.attributes_for(:video))
+    video1 = category.videos.create(Fabricate.attributes_for(:video))
     sign_in_user bob
 
-    find("a[href='#{video_path(video)}']").click
+    find("a[href='/videos/#{video1.id}']").click
     click_link "+ My Queue"
-    expect(page).to have_content(video.title)
+    expect(page).to have_content(video1.title)
 
-    click_link video.title
-    expect(page).to have_content(video.title)
+    click_link video1.title
+    expect(page).to have_content(video1.title)
     expect(page).to have_no_content("+ My Queue")
 
-    video1 = Fabricate(:video)
-    visit video_path(video1)
+    video2 = category.videos.create(Fabricate.attributes_for(:video))
+    visit home_path
+    find("a[href='/videos/#{video2.id}']").click
     click_link "+ My Queue"
 
-    video2 = Fabricate(:video)
-    visit video_path(video2)
+    video3 = category.videos.create(Fabricate.attributes_for(:video))
+    visit home_path
+    find("a[href='/videos/#{video3.id}']").click
     click_link "+ My Queue"
 
-    fill_in "video_#{video.id}",  with: 5
-    fill_in "video_#{video1.id}", with: 2
-    fill_in "video_#{video2.id}", with: 3
-
+    find("input[data-video-id='#{video1.id}']").set(5)
+    find("input[data-video-id='#{video2.id}']").set(4)
+    find("input[data-video-id='#{video3.id}']").set(2)
     click_on "Update Instant Queue"
 
-    expect(find("#video_#{video.id}").value).to eq("3")
-    expect(find("#video_#{video1.id}").value).to eq("1")
-    expect(find("#video_#{video2.id}").value).to eq("2")
+    expect(find("input[data-video-id='#{video1.id}']").value).to eq("3")
+    expect(find("input[data-video-id='#{video2.id}']").value).to eq("2")
+    expect(find("input[data-video-id='#{video3.id}']").value).to eq("1")
   end
 end
 

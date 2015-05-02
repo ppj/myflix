d815c62766fb97472fecf508839dca8a6611dd37
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-05-02 22:16:00 +1000

    reuse spec macros

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -4,7 +4,7 @@ feature "User Signs In" do
   scenario "with existing credentials" do
     bob = Fabricate(:user)
     sign_in_user bob
-    expect(page).to have_content "You have successfully logged in."
-    expect(page).to have_content bob.fullname
+    expect_to_find "You have successfully logged in."
+    expect_to_find bob.fullname
   end
 end

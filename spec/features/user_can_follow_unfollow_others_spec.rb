5ba535b3e69a3774c0e3e05b2ded7f309268541c
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-05-07 10:34:08 +1000

    clean up

--- spec/features/user_can_follow_unfollow_others_spec.rb
+++ spec/features/user_can_follow_unfollow_others_spec.rb
@@ -2,31 +2,26 @@ require 'spec_helper'
 
 feature "Social networking feature" do
   scenario "user can follow, unfollow another user" do
-    bob = Fabricate :user
     nat = Fabricate :user
     category = Fabricate :category
     video1 = category.videos.create Fabricate.attributes_for(:video)
     review = Fabricate :review, creator: nat, video: video1
 
-    sign_in_user bob
+    sign_in_user
     visit home_path
-    click_video_image video1
+    click_video_image_on_home_page video1
     click_on nat.fullname
     click_on "Follow"
 
     click_on "People"
     expect_to_find nat.fullname
 
-    click_unfollow_icon nat
-    expect_to_not_find "//tr[contains(., '#{nat.fullname}')]", "xpath"
+    unfollow nat
+    expect_to_not_find "//tr[contains(., '#{nat.fullname}')]", :xpath
   end
 end
 
-def click_video_image(video)
-  find("a[href='/videos/#{video.id}']").click
-end
-
-def click_unfollow_icon(user)
+def unfollow(user)
   within(:xpath, "//tr[contains(., '#{user.fullname}')]") do
     find("a[data-method='delete']").click
   end

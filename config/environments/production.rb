f8ccd621c016fc808850f6421297c63951799bd8
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-05-15 11:51:58 +1000

    complete url in the password reset email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { host: 'myflix-pj.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',

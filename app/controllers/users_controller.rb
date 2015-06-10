08ef52f48e2c4d84b7575a9dec5debd3d39aaab1
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-06-10 21:49:27 +1000

    handling invitation in a method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,12 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      if params[:invitation_token]
-        invitation = Invitation.find_by(token: params[:invitation_token])
-        invitation.inviter.follow(@user)
-        @user.follow(invitation.inviter)
-        invitation.update_column(:token, nil)
-      end
+      handle_invitation
       flash[:success] = "You have successfully registered. You can sign in now!"
       AppMailer.welcome_email(@user).deliver
       redirect_to sign_in_path
@@ -41,5 +36,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit!
+  end
+
+  def handle_invitation
+    if params[:invitation_token]
+      invitation = Invitation.find_by(token: params[:invitation_token])
+      invitation.inviter.follow(@user)
+      @user.follow(invitation.inviter)
+      invitation.update_column(:token, nil)
+    end
   end
 end

c75840d4e4b08da2d8b14b4c519a22a9ff353bb8
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-06-10 22:47:49 +1000

    extracted methods for readability

--- spec/features/friend_invitation_spec.rb
+++ spec/features/friend_invitation_spec.rb
@@ -4,36 +4,48 @@ feature "friend invitation" do
   scenario "user can invite a friend to join myflix" do
     jane = Fabricate :user
     sign_in_user jane
-    click_on "Welcome, #{jane.fullname}"
-    click_on "Send Invite"
 
-    fill_in "Friend's Name", with: "Joe Doe"
-    fill_in "Friend's Email Address", with: "joe@example.com"
-    fill_in "Invitation Message", with: "Join MyFlix and enjoy!"
-    click_on "Send Invitation"
+    invite_a_friend
+    friend_accepts_invitation
+    friend_signs_in
 
-    sign_out_user
-
-    open_email "joe@example.com"
-    current_email.click_link "Click here to accept the invitation."
-
-    fill_in "Password", with: "friend_password"
-    click_on "Sign Up"
-
-    fill_in "Email Address", with: "joe@example.com"
-    fill_in "Password", with: "friend_password"
-    click_on "Sign in"
-    expect_to_find "Welcome, Joe Doe"
-
-    click_on "People"
-    expect_to_find jane.fullname
-
-    sign_out_user
-
-    sign_in_user jane
-    click_on "People"
-    expect_to_find "Joe Doe"
+    friend_should_follow(jane)
+    inviter_should_follow_friend(jane)
 
     clear_emails
   end
+end
+
+def invite_a_friend
+  visit new_invitation_path
+  fill_in "Friend's Name", with: "Joe Doe"
+  fill_in "Friend's Email Address", with: "joe@example.com"
+  fill_in "Invitation Message", with: "Join MyFlix and enjoy!"
+  click_on "Send Invitation"
+  sign_out_user
+end
+
+def friend_accepts_invitation
+  open_email "joe@example.com"
+  current_email.click_link "Click here to accept the invitation."
+  fill_in "Password", with: "friend_password"
+  click_on "Sign Up"
+end
+
+def friend_signs_in
+  fill_in "Email Address", with: "joe@example.com"
+  fill_in "Password", with: "friend_password"
+  click_on "Sign in"
+end
+
+def friend_should_follow(inviter)
+  click_on "People"
+  expect_to_find inviter.fullname
+  sign_out_user
+end
+
+def inviter_should_follow_friend(inviter)
+  sign_in_user inviter
+  click_on "People"
+  expect_to_find "Joe Doe"
 end

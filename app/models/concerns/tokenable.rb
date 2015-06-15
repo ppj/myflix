fd16b201218789edcc9888e2c6a4f1b773d926db
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-06-15 23:03:38 +1000

    create token before creating the record

--- app/models/concerns/tokenable.rb
+++ app/models/concerns/tokenable.rb
@@ -2,12 +2,12 @@ module Tokenable
   extend ActiveSupport::Concern
 
   included do
-    after_create :generate_token
+    before_create :generate_token
 
     private
 
     def generate_token
-      update_column :token, SecureRandom.urlsafe_base64
+      self.token = SecureRandom.urlsafe_base64
     end
   end
 end

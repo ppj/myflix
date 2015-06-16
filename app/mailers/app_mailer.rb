9dfd3b67a5e120b348337bd5b6ecd3591acdd132
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-06-16 20:35:11 +1000

    set a default from address

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,18 @@
 class AppMailer < ActionMailer::Base
+  default from: 'info@myflix.com'
+
   def welcome_email(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: "Welcome to MyFlix!"
+    mail to: user.email, subject: "Welcome to MyFlix!"
   end
 
   def password_reset_link(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: "Password Reset Link"
+    mail to: user.email, subject: "Password Reset Link"
   end
 
   def invitation_email(invitation)
     @invitation = invitation
-    mail from: 'info@myflix.com', to: @invitation.invitee_email, subject: "Invitation to join MyFlix.com"
+    mail to: @invitation.invitee_email, subject: "Invitation to join MyFlix.com"
   end
 end

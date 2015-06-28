0bf50515124d7905e5a5d7a833c237185b50b996
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-06-28 22:30:21 +1000

    an admin has to be logged in first

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,12 +1,9 @@
 class AdminsController < ApplicationController
+  before_action :require_user
   before_action :require_admin
 
   def require_admin
-    if logged_in?
-      access_denied "This action is reserved for administrators only." unless current_user.admin?
-    else
-      require_user
-    end
+    access_denied "You are not authorized to do that." unless current_user.admin?
   end
 
 end

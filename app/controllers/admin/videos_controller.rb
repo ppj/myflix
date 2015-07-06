e671dca31696a4a10984de06d40771a35e046936
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-07-06 23:33:17 +1000

    new video title in flash message

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,7 +6,7 @@ class Admin::VideosController < AdminsController
   def create
     @video = Video.new video_params
     if @video.save
-      flash[:success] = "Video created successfully."
+      flash[:success] = "Video '#{@video.title}' added successfully."
       redirect_to new_admin_video_path
     else
       flash.now[:danger] = "Please fix the errors in the form"

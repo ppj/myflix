661aa40d1620755bc0a0dabbadf17188fe972121
Prasanna Joshi <prasanna_3003@yahoo.com>
2015-08-19 00:19:05 +1000

    add video_url to view video from show page

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -17,6 +17,6 @@ class Admin::VideosController < AdminsController
   private
 
   def video_params
-    params.require(:video).permit(:title, :category_id, :description, :small_cover, :large_cover)
+    params.require(:video).permit(:title, :category_id, :description, :small_cover, :large_cover, :video_url)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721113916) do
+ActiveRecord::Schema.define(version: 20150818133543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150721113916) do
     t.integer  "category_id"
     t.string   "small_cover"
     t.string   "large_cover"
+    t.string   "video_url"
   end
 
 end

8ff0b81a99fab57965173faa9094e9b4d64998f7
Prasanna Joshi <prasanna_3003@yahoo.com>
2016-03-22 23:41:15 +1100

    Enable Stripe subscription for user sign up

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -25,4 +25,31 @@ module StripeWrapper
       @response.present?
     end
   end
+
+  class Customer
+    attr_reader :error_message
+
+    def initialize(response, error_message)
+      @response = response
+      @error_message = error_message
+    end
+
+    def self.create(email:, source:, description:)
+      begin
+        response = Stripe::Customer.create(
+          email: email,
+          plan: :base,
+          source: source,
+          description: description,
+        )
+        new(response, nil)
+      rescue Stripe::CardError => e
+        new(nil, e.message)
+      end
+    end
+
+    def successful?
+      @response.present?
+    end
+  end
 end
--- app/services/user_signup.rb
+++ app/services/user_signup.rb
@@ -14,11 +14,11 @@ class UserSignup
 
   def perform
     if user.valid?
-      charge_result = charge_credit_card
-      if charge_result.successful?
+      new_subscription = subscribe_new_customer
+      if new_subscription.successful?
         user_sign_up_success_steps
       else
-        @error_message = charge_result.error_message
+        @error_message = new_subscription.error_message
       end
     else
        @error_message = "Please fix the highlighted errors before continuing..."
@@ -34,11 +34,11 @@ class UserSignup
 
   attr_reader :user, :invitation_token, :stripe_token
 
-  def charge_credit_card
-    StripeWrapper::Charge.create(
-      amount: 999,
+  def subscribe_new_customer
+    StripeWrapper::Customer.create(
+      email: user.email,
       source: stripe_token,
-      description: "MyFlix Sign Up Charge for #{user.email}"
+      description: "MyFlix subscription for #{user.email}"
     )
   end
 

1672422b624aaf0fcd8ecbd241f1079777956cc8
Prasanna Joshi <prasanna_3003@yahoo.com>
2016-04-11 00:38:44 +1000

    Filter search based on average rating of a video

--- app/models/video.rb
+++ app/models/video.rb
@@ -35,11 +35,23 @@ class Video < ActiveRecord::Base
       search_definition[:query][:multi_match][:fields] << "reviews.body"
     end
 
+    if options[:rating_from] || options[:rating_to]
+      search_definition[:filter] = {
+        range: {
+          rating: {
+            gte: (options[:rating_from] if options[:rating_from].present?),
+            lte: (options[:rating_to] if options[:rating_to].present?),
+          }
+        }
+      }
+    end
+
     __elasticsearch__.search(search_definition)
   end
 
   def as_indexed_json(options={})
     as_json(
+      methods: :rating,
       only: [:title, :description],
       include: {
         reviews: { only: :body }
